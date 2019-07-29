3a3eb724095453e2a9afe293a7d19b2949163a00
Max Coplan <mchcopl@gmail.com>
2019-07-29 18:30:40 -0400

    Compact code, can download live list

--- api.py
+++ api.py
@@ -1,34 +1,75 @@
 #!/usr/bin/env python3
 
-import youtube_dl
 import re
-#ydl_opts = {
-#    'simulate'
-#}
-#
-#with youtube_dl.YoutubeDL(ydl_opts) as ydl:
-#    ydl.download(['https://www.youtube.com/playlist?list=UUzQUP1qoWDoEbmsQxvdjxgQ&playnext=1&index=1'])
+import numpy as np
+import pandas as pd
+import youtube_dl
 
-ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
+def getTitles():
+    ydl = youtube_dl.YoutubeDL({'outtmpl': '%(id)s%(ext)s'})
 
-with ydl:
-    result = ydl.extract_info(
-        'https://www.youtube.com/playlist?list=PLk1Sqn_f33KuS7ZSVMJqzFaqOyyl-esmG',
-        download=False # We just want to extract the info
-    )
+    with ydl:
+        ydl.params.update(ignoreerrors=True)
+        result = ydl.extract_info(
+            'https://www.youtube.com/playlist?list=UUzQUP1qoWDoEbmsQxvdjxgQ',
+            download = False # We just want to extract the info
+        )
 
-if 'entries' in result:
-    # Can be a playlist or a list of videos
-    video = result['entries'][0]
-else:
-    # Just a video
-    video = result
+    titles = []
+    for t in range(len(result['entries'])):
+        try:
+            titles.append(result['entries'][t]['title'])
+        except:
+            pass        
+    out = open('titles.txt', 'w')
+    
+    for title in titles:
+        out.write('%s\n' %title)
 
-titles=[]
-for t in range(len(result['entries'])):
-    titles.append(result['entries'][t]['title'])
-print(titles)
+    return titles
 
-p=re.compile(r'^.*?- (?P<name>.*)$')
-for title in titles:
-    print(p.search(title).group('name'))
+def getNames(titles):
+    names = []
+    p = re.compile(r'^.*?#.*?(-|with)\s*?(?P<name>.*?)(\(.*?\))?$')
+    for title in titles:
+        try:
+            allGuests = re.split(',|&', p.match(title).group('name'))
+            for person in allGuests:
+                person = person.strip()
+                names.append(person)
+        except Exception as err:
+            #print(err)
+            #print(title)
+            pass
+    return names
+
+def count(names):
+    npNames = np.array(names)
+    sortedNames = np.sort(npNames)
+    res = pd.value_counts(sortedNames, sort=True)
+    nRes = np.transpose(np.array([res.keys().to_numpy(),res.to_numpy()]))
+    return nRes
+
+def removeSingles(arr):
+    delRange = []
+    for i in range(len(arr)):
+        if arr[i][1] == 1:
+            delRange.append(i)
+    arr = np.delete(arr, delRange, 0)
+    return arr
+
+def main():
+    #Uncomment to download list from web (slow)
+    #titles = getTitles()
+
+    #Uncomment below to use cached list (fast)
+    titles = open('allTitles.txt')
+
+    names = getNames(titles)
+    viewCounts = count(names)
+    relevantCounts = removeSingles(viewCounts)
+    print(relevantCounts)
+
+
+if __name__ == '__main__':
+    main()
--- vcheck.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-
-import pandas as pd
-import numpy as np
-import scipy as sp
-
-print(pd.__version__)
-print(np.__version__)
-print(sp.__version__)
